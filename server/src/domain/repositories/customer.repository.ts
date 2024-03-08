dac6773a63745bdf37ac5019e0eac306afcd8b4e
Matheus <ymatheus.h@gmail.com>
2024-03-08 03:50:26 -0300

    refactor: :recycle: Refactor customer repository and service

--- server/src/domain/repositories/customer.repository.ts
+++ server/src/domain/repositories/customer.repository.ts
@@ -1,12 +1,13 @@
 import { pool } from "../../database/connection";
 import { ICreateCustomerDTO } from "../dto/ICreateCustomerDTO";
 import { ICustomerResponseDTO } from "../dto/ICustomerResponseDTO";
+import { ErrorTypes } from "../errors/errors";
 
 class CustomerRepository {
   async create({ name, email, phone, x, y }: ICreateCustomerDTO) {
-    const query =
-      "INSERT INTO customers (name, email, phone, x, y) VALUES ($1, $2, $3, $4, $5) RETURNING *";
     try {
+      const query =
+        "INSERT INTO customers (name, email, phone, x, y) VALUES ($1, $2, $3, $4, $5) RETURNING *";
       const { rows } = await pool.query<ICustomerResponseDTO>(query, [
         name,
         email,
@@ -17,30 +18,32 @@ class CustomerRepository {
 
       return rows[0];
     } catch (error) {
-      console.error(error);
+      throw ErrorTypes.CreateCustomerError;
     }
   }
 
   async getAll() {
-    const query = "SELECT id, name, email, phone, x, y FROM customers";
     try {
+      const query = "SELECT id, name, email, phone, x, y FROM customers";
       const { rows } = await pool.query<ICustomerResponseDTO>(query);
 
       return rows;
     } catch (error) {
-      console.error(error);
+      throw ErrorTypes.GetCustomerError;
     }
   }
 
   async searchByField(field: string) {
-    const query =
-      "SELECT id, name, email, phone, x, y FROM customers WHERE name LIKE $1 OR email LIKE $1 OR phone LIKE $1";
     try {
-      const { rows } = await pool.query<ICustomerResponseDTO>(query, [`%${field}%`]);
+      const query =
+        "SELECT id, name, email, phone, x, y FROM customers WHERE name LIKE $1 OR email LIKE $1 OR phone LIKE $1";
+      const { rows } = await pool.query<ICustomerResponseDTO>(query, [
+        `%${field}%`,
+      ]);
 
       return rows;
     } catch (error) {
-      console.error(error);
+      throw ErrorTypes.SearchCustomerError;
     }
   }
 }
--- server/src/domain/services/customer.service.ts
+++ server/src/domain/services/customer.service.ts
@@ -6,23 +6,17 @@ class CustomerService {
   async create(customer: ICreateCustomerDTO) {
     const customerCreated = await customerRepository.create(customer);
 
-    if (!customerCreated) throw new Error('Error creating customer')
-
     return customerCreated.id;
   }
 
   async getAll() {
     const getAllCustomers = await customerRepository.getAll();
 
-    if (!getAllCustomers) throw new Error('Error get all customers')
-
     return getAllCustomers;
   }
 
   async searchByField(field: string) {
     const searchCustomers = await customerRepository.searchByField(field);
-
-    if (!searchCustomers) throw new Error('Error search customers')
 
     return searchCustomers;
   }
