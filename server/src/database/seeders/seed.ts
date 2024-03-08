94003c5e07e82205cac4cfdd140cc28b81785c84
Matheus <ymatheus.h@gmail.com>
2024-03-08 01:40:59 -0300

    feat: :sparkles: Add seed script for database initialization

---
+++ server/src/database/seeders/seed.ts
@@ -0,0 +1,35 @@
+import fs from "node:fs";
+import path from "node:path";
+import { Client } from "pg";
+
+export default async function seedDatabase(connection: Client) {
+  try {
+    const filePath = path.resolve(__dirname, "seed.sql");
+
+    const SQLContent = fs.readFileSync(filePath).toString();
+
+    const queries = SQLContent.split(";").filter((p) => p.trim());
+
+    for (let i = 0; i < queries.length; i += 1) {
+      const query = queries[i];
+      await connection.query(query);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+(async () => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+  await client.connect();
+
+  try {
+    await seedDatabase(client)
+    console.log("Database seeded successfully!");
+    process.exit(0);
+  } finally {
+    client.end()
+  }
+})().catch(e => console.error(e.message, e.stack))
