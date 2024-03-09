6936beef2f379ba3be3660266d41bd9dd594510d
Matheus <ymatheus.h@gmail.com>
2024-03-08 23:15:24 -0300

    refactor: :recycle: Refactor customer controller and add CORS configuration

--- server/src/http/controllers/customer.controller.ts
+++ server/src/http/controllers/customer.controller.ts
@@ -2,40 +2,54 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
 import customerService from "../../domain/services/customer.service";
 
-const PHONE_REGEX =
-  /^(?:(?:\+|00)?(55)\s?)?(?:(?:\(?[1-9][0-9]\)?)?\s?)?(?:((?:9\d|[2-9])\d{3})-?(\d{4}))$/;
-
 export default class CustomerController {
   static async create(request: FastifyRequest, reply: FastifyReply) {
     const createCustomerBody = z.object({
-      name: z.string(),
+      name: z.string().min(3),
       email: z.string().email(),
-      phone: z.string().regex(PHONE_REGEX, "Invalid phone number"),
-      x: z.number().int(),
-      y: z.number().int(),
+      phone: z
+        .string()
+        .regex(
+          /^\([1-9]{2}\) (?:[2-8]|9[0-9])[0-9]{3}\-[0-9]{4}$/,
+          "Invalid phone number"
+        ),
+      x: z.coerce
+        .number()
+        .int()
+        .refine((value) => value !== 0, { message: "X cannot be 0" }),
+      y: z.coerce
+        .number()
+        .int()
+        .refine((value) => value !== 0, { message: "X cannot be 0" }),
     });
 
     const { name, email, phone, x, y } = createCustomerBody.parse(request.body);
 
-    const customerId = await customerService.create({ name, email, phone, x, y });
+    const customerId = await customerService.create({
+      name,
+      email,
+      phone,
+      x,
+      y,
+    });
 
     return reply.status(201).send({ customerId });
   }
 
-  static async getAll(_request: FastifyRequest, reply: FastifyReply) {
-    const customers = await customerService.getAll();
-
-    return reply.send(customers);
-  }
-
-  static async searchByField(request: FastifyRequest, reply: FastifyReply) {
+  static async get(request: FastifyRequest, reply: FastifyReply) {
     const customerQuerySchema = z.object({
-      search: z.string(),
-    })
+      search: z.string().optional(),
+    });
 
     const { search } = customerQuerySchema.parse(request.query);
 
-    const customers = await customerService.searchByField(search);
+    if (search) {
+      const customers = await customerService.searchByField(search);
+
+      return reply.send(customers);
+    }
+
+    const customers = await customerService.getAll();
 
     return reply.send(customers);
   }
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,13 +1,19 @@
+import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
 import CustomerController from "./controllers/customer.controller";
 import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = fastify();
 
+app.register(fastifyCors, {
+  origin: ['http://localhost:3000'],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+});
+
 app.setErrorHandler(errorMiddleware);
 
 app.post("/customer", CustomerController.create);
-app.get("/customer", CustomerController.getAll);
-app.get("/customer/search", CustomerController.searchByField);
+app.get("/customer", CustomerController.get);
 
 app.listen({ port: 3333 }).then(() => console.log("HTTP server running!"));
