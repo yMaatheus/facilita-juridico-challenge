e012e8e61d16e5f11f81655042909d5706c4a9b2
Matheus <ymatheus.h@gmail.com>
2024-03-09 13:44:27 -0300

    style: :lipstick: Update API base URL in .env and env.ts files

--- client/src/lib/env.ts
+++ client/src/lib/env.ts
@@ -5,12 +5,10 @@ export const env = createEnv({
   server: {},
 
   client: {
-    NEXT_PUBLIC_BASE_URL: z.string().url(),
     NEXT_PUBLIC_API_BASE_URL: z.string().url(),
   },
 
   runtimeEnv: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
 })
